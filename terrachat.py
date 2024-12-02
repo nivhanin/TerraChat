b3444c4f5f5e4626bcc0086964cee69cea46ca22
nhanin <nhanin@cisco.com>
2024-12-02 15:47:06 +0200

    feat: major version upgrade * add cooldown handling per minute & day * addd new model: Gemini Pro 1.5 * add new model: Cohere Command R * add relevant LLM icon for each TerraChat response * style fixes

--- terrachat.py
+++ terrachat.py
@@ -30,6 +30,14 @@ available_tools = [
 
 # Create LLM instance using Langchain
 llm_instance_mistral = ChatMistralAI(model_name="open-mistral-nemo")
+llm_gemini_ai_instance_pro = ChatGoogleGenerativeAI(
+    model="models/gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
 llm_gemini_ai_instance_flash = ChatGoogleGenerativeAI(
     model="models/gemini-1.5-flash",
     temperature=0,
@@ -46,22 +54,28 @@ llm_gemini_ai_instance_flash_8b = ChatGoogleGenerativeAI(
     max_retries=2,
     # other params...
 )
-llm_cohere = ChatCohere(model="command-r-plus-08-2024")
+llm_cohere_plus = ChatCohere(model="command-r-plus-08-2024")
+llm_cohere = ChatCohere(model="command-r-08-2024")
+
 
 # map model names to display names
 model_display_names = {
     "open-mistral-nemo": "MistralAI",
+    "models/gemini-1.5-pro": "Gemini AI Pro",
     "models/gemini-1.5-flash": "Gemini AI Flash",
     "models/gemini-1.5-flash-8b": "Gemini AI Flash 8B",
-    "command-r-plus-08-2024": "Cohere",
+    "command-r-plus-08-2024": "Cohere+",
+    "command-r-08-2024": "Cohere",
 }
 
 # map the model to the images
 model_images = {
     "open-mistral-nemo": "mistralai.png",
+    "models/gemini-1.5-pro": "gemini_pro.png",
     "models/gemini-1.5-flash": "gemini.png",
-    "models/gemini-1.5-flash-8b": "gemini.png",
-    "command-r-plus-08-2024": "cohere.png",
+    "models/gemini-1.5-flash-8b": "gemini_8b.png",
+    "command-r-plus-08-2024": "cohere_plus.png",
+    "command-r-08-2024": "cohere.png",
 }
 
 # Initialize components for dynamic message retrieval - Memory feature
@@ -73,7 +87,7 @@ vectorstore = InMemoryVectorStore(mistral_embeddings)
 
 
 @retry(
-    stop=stop_after_attempt(3),
+    stop=stop_after_attempt(10),
     wait=wait_fixed(2),
     retry=retry_if_exception_type(KeyError),
 )
@@ -125,31 +139,43 @@ parser = StrOutputParser()
 
 # Setup chains - Create LLM chain
 mistral_chain = prompt_template | llm_instance_mistral | parser
+gemini_ai_pro_chain = prompt_template | llm_gemini_ai_instance_pro | parser
 gemini_ai_flash_chain = prompt_template | llm_gemini_ai_instance_flash | parser
 gemini_ai_flash_8b_chain = prompt_template | llm_gemini_ai_instance_flash_8b | parser
+cohere_plus_chain = prompt_template | llm_cohere_plus | parser
 cohere_chain = prompt_template | llm_cohere | parser
 
-
-# Create LLMChain instances
-llm_chains = [
-    # Order of chains is important
-    RateLimiterLLMChain(llm_chain=mistral_chain, max_requests_per_minute=30),
-    RateLimiterLLMChain(
-        llm_chain=gemini_ai_flash_chain,
-        max_requests_per_minute=60,
-        max_requests_per_day=1,
-    ),
-    RateLimiterLLMChain(
-        llm_chain=gemini_ai_flash_8b_chain,
-        max_requests_per_minute=15,
-        max_requests_per_day=500,
-    ),
-    RateLimiterLLMChain(
-        llm_chain=cohere_chain,
-        max_requests_per_minute=20,
-        max_requests_per_day=15,
-    ),
-]
+if "llm_chains" not in st.session_state:
+    # Create LLMChain instances
+    st.session_state.llm_chains = [
+        Order of chains is important
+        RateLimiterLLMChain(
+            llm_chain=gemini_ai_pro_chain,
+            max_requests_per_minute=1,  # 2
+            max_requests_per_day=1,  # 50
+        ),
+        RateLimiterLLMChain(
+            llm_chain=gemini_ai_flash_chain,
+            max_requests_per_minute=1,  # 15
+            max_requests_per_day=1,  # 1500
+        ),
+        RateLimiterLLMChain(
+            llm_chain=gemini_ai_flash_8b_chain,
+            max_requests_per_minute=1,  # 15
+            max_requests_per_day=1,  # 1500
+        ),
+        RateLimiterLLMChain(
+            llm_chain=cohere_plus_chain,
+            max_requests_per_minute=1,  # 20
+            max_requests_per_day=1,  # 1000 per month (approx 32 per day)
+        ),
+        RateLimiterLLMChain(
+            llm_chain=cohere_chain,
+            max_requests_per_minute=1,  # 20
+            max_requests_per_day=1,  # 1000 per month (approx 32 per day)
+        ),
+        RateLimiterLLMChain(llm_chain=mistral_chain, max_requests_per_minute=4),  # 30
+    ]
 
 # Initialize chat history
 if "chat_history" not in st.session_state:
@@ -158,7 +184,9 @@ if "chat_history" not in st.session_state:
 # Display chat messages from history on app rerun
 for session_message in st.session_state.chat_history:
     log.info(f"Session message: {session_message}")
-    with st.chat_message(session_message["role"], avatar=session_message.get("avatar", None)):
+    with st.chat_message(
+        session_message["role"], avatar=session_message.get("avatar", None)
+    ):
         st.markdown(session_message["content"])
 
 
@@ -190,6 +218,10 @@ def run_chains_with_function_result(result, chains):
 
 def generate_response(user_prompt, chains):
     for llm_chain in chains:
+        if llm_chain.is_in_cooldown():
+            log.info(f"Skipping {llm_chain.model_name} due to cooldown.")
+            continue
+
         response = llm_chain.run_invoke(user_prompt)
         if response:
             model_name = llm_chain.model_name
@@ -232,7 +264,7 @@ def process_response(response):
 
         log.info(f" -- running {function_name} {function_params}")
         result = available_actions[function_name](**function_params)
-        response = run_chains_with_function_result(result, llm_chains)
+        response = run_chains_with_function_result(result, st.session_state.llm_chains)
         turn_count += 1
     return response
 
@@ -244,9 +276,15 @@ def main():
         user_prompt = handle_user_input(user_question)
         # Display spinner while generating response
         # Generate response
-        response = generate_response(user_prompt, llm_chains)
+        response = generate_response(user_prompt, st.session_state.llm_chains)
         with st.spinner("Assistant is thinking..."):
-            with st.chat_message("ai", avatar=f"images/{model_images[st.session_state.model_name]}"):
+            log.info(f"!!! st session state: {st.session_state} !!!")
+            ai_avatar = (
+                f"images/{model_images[st.session_state.model_name]}"
+                if model_images[st.session_state.model_name]
+                else ""
+            )
+            with st.chat_message("ai", avatar=ai_avatar):
                 log.info(f"Final response: {response}")
                 # Add the AI response to history and inform the user
                 try:
@@ -254,11 +292,16 @@ def main():
                 except IndexError:
                     answer_data = response
                     log.error("IndexError: No answer data found!")
+
                 add_message_to_history(
-                    "ai", answer_data, ai_avatar=f"images/{model_images[st.session_state.model_name]}")
+                    "ai",
+                    answer_data,
+                    ai_avatar=ai_avatar,
+                )
                 st.markdown(answer_data)
                 st.caption(
-                    f"Source: {model_display_names[st.session_state.model_name]} ({st.session_state.model_name})")
+                    f"Source: {model_display_names[st.session_state.model_name]} ({st.session_state.model_name})"
+                )
 
 
 if __name__ == "__main__":
