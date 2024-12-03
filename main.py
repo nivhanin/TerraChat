93c11be428703544032a0982f7f7d8180ec31d36
idora <idora@cisco.com>
2024-12-03 12:01:14 +0200

    feat: enhance chat history management and response handling

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import sys
 from streamlit.web import cli as stcli
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.argv = ["streamlit", "run", "terrachat.py"]
     sys.exit(stcli.main())
